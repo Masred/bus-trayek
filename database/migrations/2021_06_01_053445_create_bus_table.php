dedcd33f658b8dd41c56118de1a9798575af3d4f
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-06-01 19:52:30 +0700

    table migration for bus table

---
+++ database/migrations/2021_06_01_053445_create_bus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_trayek')->nullable()->constrained('trayek');
+            $table->integer('kapasitas')->nullable();
+            $table->integer('total_penumpang')->nullable();
+            $table->string('status')->nullable();
+            $table->string('cam_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus');
+    }
+}
